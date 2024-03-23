9ed2417281a4f2c996c7a3ff08d1b3454e4ae235
Georgy Koval-Volkov <koval-volkov@yandex.ru>
2024-03-23 21:21:47 +0700

    Ver.1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardSection = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function createCard(cardFilling, cardDeleting) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = cardFilling.name;
+    cardElement.querySelector('.card__image').src = cardFilling.link;
 
-// @todo: Вывести карточки на страницу
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', function() {
+        cardDeleting(cardElement);
+    });
+
+    return cardElement;
+};
+
+function cardDeleting (element) {
+    element.remove ();
+};
+
+initialCards.forEach((data) => {
+  const insertCard = createCard(data, cardDeleting);
+  cardSection.append(insertCard);
+}
+);
