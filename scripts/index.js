4885508b793002cc05d147b177c7de8c5e7e120d
Georgy Koval-Volkov <koval-volkov@yandex.ru>
2024-03-23 22:00:19 +0700

    Ver.1.1

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,26 @@ const cardSection = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
 
-function createCard(cardFilling, cardDeleting) {
+function createCard(cardFilling, deleteCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     cardElement.querySelector('.card__title').textContent = cardFilling.name;
     cardElement.querySelector('.card__image').src = cardFilling.link;
+    cardElement.querySelector('.card__image').alt = cardFilling.name;
 
     const cardDeleteButton = cardElement.querySelector('.card__delete-button');
     cardDeleteButton.addEventListener('click', function() {
-        cardDeleting(cardElement);
+        deleteCard(cardElement);
     });
 
     return cardElement;
 };
 
-function cardDeleting (element) {
+function deleteCard (element) {
     element.remove ();
 };
 
 initialCards.forEach((data) => {
-  const insertCard = createCard(data, cardDeleting);
+  const insertCard = createCard(data, deleteCard);
   cardSection.append(insertCard);
 }
 );
